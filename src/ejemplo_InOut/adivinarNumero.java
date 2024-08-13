4392f1893f8f0c76dc7b71c7d71289c3fc6df834
guillermo-diaz <guillermo.diaz@est.fi.uncoma.edu.ar>
2024-08-13 17:10:04 -0300

    ex04 part2

---
+++ src/ejemplo_InOut/adivinarNumero.java
@@ -0,0 +1,31 @@
+package ejemplo_InOut;
+import java.util.Random;
+import java.util.Scanner;
+
+public class adivinarNumero {
+    
+    static int inicio = 0;
+    static int fin = 100;
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int r = random.nextInt(fin-inicio)+inicio; //nro ranodm entre [inicio, fin]
+        int n = -1;
+        
+        while(n != r) {
+            System.out.printf("Adivine el numero entre %d y %d: \n",inicio, fin);
+            n = scanner.nextInt();
+            if(n < r){
+                System.out.printf("El numero es mayor que %d.\n", n);
+                // inicio = n;    si quiere ir cambiando el rango
+            } else if (n > r){ 
+                System.out.printf("El numero es menor que %d.\n", n);
+                // fin = n;         
+            }
+        }
+        
+        System.out.printf("¡Acertaste! El nro es %d! \n", n);
+        scanner.close();
+    }
+}
